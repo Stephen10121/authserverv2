856ee09eb706569e22223d504366752daa2e5813
Stephen Gruzin <stephengrzn@gmail.com>
2022-10-06 16:36:02 -0700

    Added startup script

---
+++ backend/src/startup.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 18; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
+fs.writeFile('./src/.env', `ACCESS_TOKEN_SECRET=${makeid()}\nREFRESH_TOKEN_SECRET=${makeid()}`,  function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("File created!");
+});
